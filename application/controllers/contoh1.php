52fbf19d60e461b2c447901eeb116d8e6e4def44
yuliaa23 <yuliaagustin2308@gmail.com>
2022-03-25 14:48:20 +0700

    menambahkan contoh 1,2 dan 3 yaitu = contoh1.php,latihan1.php,model_latihan1.php,view-latihan.php beserta isinya

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Yulia Agustin
+            Saya tinggal di daerah Kalideres
+            olah raga yang saya sukai adalah
+            Bulutangkis";
+    }
+}
