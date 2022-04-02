f95c0bfc8df52c314ea9eeb0e390a3db019d1d9a
yuliaa23 <yuliaagustin2308@gmail.com>
2022-04-02 13:41:19 +0700

    menambahkan yaitu Web.php di dalam application/controllers,v_header.php,v_index.php,v_footer.php,v_about.php di dalam application/view,stylebuku.css di dalam assets/css,config.php

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1> 
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Yulia</li>
+        <li>Nama Belakang : Agustin</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> jalan duri kosambi</li>
+    </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Pandeglang</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Catur</li>
+    </ul>
+</section>
